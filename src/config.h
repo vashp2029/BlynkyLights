69e7c5ca85faeed83ccb7418998886cfc97a7a52
Vash Patel <vashp2029@yahoo.com>
2018-05-08 20:28:47 -0400

    Moved definition of PASSWORD to apikeys.h

--- src/config.h
+++ src/config.h
@@ -49,6 +49,5 @@
 	//OTA updates
 	#define OTAPORT			8266	//Listen on this port for OTA updates
 	#define HTTPPORT		80		//Listen on this port for HTTP updates
-	#define PASSWORD		"mypass"//Used for OTA, may use this for other things later
 
 #endif
