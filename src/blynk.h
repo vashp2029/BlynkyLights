6fdb86de07dec8f2f9a6ca63d5f69b8faf04122c
Vash Patel <vashp2029@yahoo.com>
2018-05-12 00:17:09 -0400

    Added TODO for blynk inputs

--- src/blynk.h
+++ src/blynk.h
@@ -10,4 +10,10 @@
 
 	#include "BlynkSimpleEsp8266.h"
 
+	//TODO Possible inputs we need from Blynk: Selection for location, on/off,
+	//TODO brightness, effect speed, mic sensitivity, selection for effects,
+	//TODO selection for sound-reactive effects (should those be separate?),
+	//TODO selction for RGB color, and some way to obtain a time to automatically
+	//TODO turn LEDs on and off.
+
 #endif
